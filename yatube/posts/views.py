d8a93a376cb40268f468410fd1a81a7790a4e5ae
Танцюра Екатерина <tantsiura.e@yandex.ru>
2022-11-12 15:24:34 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+# Страница с постами,отфильтрованными по группам
+def group_posts(request, slug):
+    return HttpResponse('Здесь будут отображены группы постов. {slug}')
+
+
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ice_cream.apps.IceCreamConfig',  # Добавленная запись
+    'posts.apps.PostsConfig',  # Добавленная запись
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
